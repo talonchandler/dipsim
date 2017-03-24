2118c359d0ee13137f3ddfa138130f7169c78d53
Talon Chandler <talonchandler@talonchandler.com>
2017-03-24 16:04:17 -0500

    Setup for comparing multiple frames.

--- examples/illum.py
+++ examples/illum.py
@@ -6,12 +6,12 @@ import time; start = time.time(); print('Running...')
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-n = 2000
+n = 200#2000
 dpi = 250
 vis_px = 2000
-bfp_rads = [1, 5, 10]
+bfp_rads = [1, 2, 3, 4]
 n_cols = len(bfp_rads)
-n_rows = 3
+n_rows = 5
 inch_fig = 5
 
 fig, axs = plt.subplots(n_rows, n_cols,
@@ -47,22 +47,19 @@ for i, bfp_rad in enumerate(bfp_rads):
 
     m.plot_intensities_from_single_fluorophore('intensity'+str(bfp_rad)+'.png',
                                                 n=n, interact=False, color_norm='linear', my_ax=axs[1][i], my_cax=caxs[1][i], dpi=dpi, vis_px=vis_px)
-
-    m.plot_intensities_from_single_fluorophore_new('intensity_new'+str(bfp_rad)+'.png',
-                                                n=n, interact=False, color_norm='linear', my_ax=axs[2][i], my_cax=caxs[2][i], dpi=dpi, vis_px=vis_px)
     
     m.draw_scene('scene'+str(bfp_rad)+'.png', interact=False, my_ax=axs[0][i], dpi=dpi, vis_px=vis_px)
     caxs[0][i].axis('off')
 
-row_labels = ['Scene', '$I\propto |\mu_{\mathrm{abs}} \cdot \int d \mathbf{r}\' \mathbf{R}(\mathbf{r}\')\mathbf{E}_{\mathrm{bfp}}|^2$', '$I\propto |\int d \mathbf{r}\'|\mu_{\mathrm{abs}}^{\dagger}\mathbf{R}(\mathbf{r}\')\mathbf{A}_{\mathrm{bfp}}||^2$']
+row_labels = ['Scene', 'Emitted Power', r'$\sigma_{\phi}$', r'$\sigma_{\theta}$', r'$\sigma_{\Omega} = \sigma_{\phi}\sigma_{\theta}\sin\theta$']
 for i, label in enumerate(row_labels):
     axs[i][0].annotate(label, xy=(0,0), xytext=(-0.1, 0.5), textcoords='axes fraction',
                        va='center', ha='center', rotation=90, fontsize=18)
 
-col_labels = [r'$\frac{\rho}{f} = 0.1$', r'$\frac{\rho}{f} = 0.5$', r'$\frac{\rho}{f} = 1.0$']
+col_labels = [r'$N=1$', r'$N=2$', r'$N=3$', r'$N=4$']
 for i, label in enumerate(col_labels):
     axs[0][i].annotate(label, xy=(0,0), xytext=(0.5, 1.1), textcoords='axes fraction',
                        va='center', ha='center', fontsize=18)
 
-fig.savefig('compare_illum.png', dpi=dpi)
+fig.savefig('compare_N_pol.png', dpi=dpi)
 print('Total time: '+str(np.round(time.time() - start, 2)))

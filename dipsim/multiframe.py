Fix inefficient CRLB calculation.

dipsim/multiframe.py
@@ -22,9 +22,9 @@ class MultiFrameMicroscope:
 
     def calc_solid_angle_min_std(self, args):
         
-        def ev_function(self, args):
-            theta = args[0]
-            phi = args[1]
+        def ev_function(self, ev_args):
+            theta = ev_args[0]
+            phi = ev_args[1]
         
             flu_dir = np.array([np.sin(theta)*np.cos(phi),
                                np.sin(theta)*np.sin(phi),
@@ -39,7 +39,7 @@ class MultiFrameMicroscope:
         
         ev_function = functools.partial(ev_function, self)
         rv = stats.RandomVariable(ev_function, dist='poisson')
-        crlb = rv.crlb(args, [0.00001, 0.00001])
+        crlb = rv.crlb(args, [1e-5, 1e-5])
         solid_angle_std = np.sin(args[0])*np.sqrt(crlb[0])*np.sqrt(crlb[1])
         
         return solid_angle_std
@@ -76,7 +76,7 @@ class NFramePolScope(MultiFrameMicroscope):
                                           f=10,
                                           bfp_rad=3,
                                           bfp_pol=bfp_pol,
-                                          bfp_n=16)
+                                          bfp_n=4)
             m.append(microscope.Microscope(illuminator=ill, detector=det))
                      
         self.microscopes = m

dipsim/stats.py
@@ -24,19 +24,25 @@ class RandomVariable:
         The length of x0 and dx must match the number of arguments of 
         self.ev_func.
         """
-        # Populate fisher information matrix
         if self.dist == 'poisson':
             f = self.ev_func
             n = len(x0)
-            xx, yy = np.meshgrid(np.arange(n), np.arange(n))
-            f_ind = np.stack((xx, yy))
 
-            def calc_fisher_ij(ind, self):
-                dx0 = np.eye(1, n, k=ind[0]).flatten()*dx
-                dx1 = np.eye(1, n, k=ind[1]).flatten()*dx
-                deriv0 = (f(x0+0.5*dx0) - f(x0-0.5*dx0))/np.sum(dx0)
-                deriv1 = (f(x0+0.5*dx1) - f(x0-0.5*dx1))/np.sum(dx1)
-                return np.sum(deriv0*deriv1/f(x0))
-            
-            f_inf = np.apply_along_axis(calc_fisher_ij, 0, f_ind, self)
-            return np.diag(np.linalg.pinv(f_inf))
+            # Evaluate function at all points (2n total evaluations)
+            # fpm (n x 2) contains the function at +/- points along each param
+            fpm = np.zeros((n, 2))
+            for i in range(n):
+                v_dx = np.eye(1, n, k=i).flatten()*dx # ith unit vector
+                fpm[i, 0] = f(x0 + 0.5*v_dx)
+                fpm[i, 1] = f(x0 - 0.5*v_dx)
+
+            # Calculate derivatives
+            derivs = (fpm[:,0] - fpm[:,1])/dx
+
+            # Calculate fisher information matrix
+            f_inf = np.outer(derivs, derivs)/f(x0)
+
+            # Invert and return diagonal
+            crlb =  np.diag(np.linalg.pinv(f_inf))
+
+            return crlb
Fix bug in detector efficiency calculation.

dipsim/detector.py
@@ -20,6 +20,6 @@ class Detector:
             A = (1.0/4.0) - (3.0/8.0)*np.cos(self.alpha) + (1.0/8.0)*(np.cos(self.alpha)**3)
             B = (3.0/16.0)*np.cos(self.alpha) - (3.0/16.0)*(np.cos(self.alpha)**3)
             theta = util.theta_prime(fluorophore.mu_em[0], fluorophore.mu_em[1], self.theta_optical_axis)
-            return A + B*(np.sin(theta)**2)
+            return 2*(A + B*(np.sin(theta)**2))
         elif self.det_type=='4pi':
             return 1.0
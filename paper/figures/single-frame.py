0bad1c892f2a09cc2dfb0688f6cebdf767bf1de7
Talon Chandler <talonchandler@talonchandler.com>
2017-07-06 14:49:25 -0500

    Added paper template and moved examples into figures folder.

---
+++ paper/figures/single-frame.py
@@ -0,0 +1,63 @@
+from dipsim import multiframe, util, detector, illuminator, microscope, util
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os; import time; start = time.time(); print('Running...')
+
+# Main input parameters
+n_pts = 10000
+
+pols = 3*[0]
+ill_axes = 3*[0]
+det_axes = 3*[0]
+ill_nas = [0.9, 1.1, 1.3]
+det_nas = [0.9, 1.1, 1.3]
+n_rows = 4
+n_cols = len(det_axes)
+inch_fig = 5
+dpi = 250
+col_labels = ['NA = 0.9', 'NA = 1.1', 'NA = 1.3']
+row_labels = ['Geometry', r'Excitation Efficiency $\eta_{\text{exc}}$', r'Detection Efficiency $\eta_{\text{det}}$', r'Total Efficiency $\eta_{\text{tot}}$']
+
+# Generate axes
+size = (inch_fig*n_cols, inch_fig*n_rows)
+fig, axs = plt.subplots(n_rows, n_cols, figsize=size)
+plt.subplots_adjust(wspace=0.2, hspace=-0.3)
+if len(pols) == 1:
+    axs = np.expand_dims(axs, 1)
+caxs = util.generate_caxs(axs)
+
+# Compute and plot on axes
+for i, (pol, det_axis, ill_axis, det_na, ill_na) in enumerate(zip(pols, det_axes, ill_axes, det_nas, ill_nas)):
+    print('Computing microscope: ' + str(i))
+
+    # Create microscope
+    ill = illuminator.Illuminator(illum_type='wide',
+                                  theta_optical_axis=ill_axis,
+                                  na=ill_na,
+                                  phi_pol=pol)
+    
+    det = detector.Detector(theta_optical_axis=np.array(det_axis),
+                            na=det_na)
+                                  
+    m = microscope.Microscope(illuminator=ill, detector=det, max_photons=1)
+    
+    # Plot scene and efficiencies
+    m.plot_excitation_efficiency(n=n_pts, my_ax=axs[1,i], my_cax=caxs[1,i],
+                                 color_min=0, color_max=1.0)
+    m.plot_collection_efficiency(n=n_pts, my_ax=axs[2,i], my_cax=caxs[2,i],
+                                 color_min=0, color_max=0.5)
+    m.plot_sensitivity(n=n_pts, my_ax=axs[3,i], my_cax=caxs[3,i],
+                                 color_min=0, color_max=0.5)
+
+    scene_string = m.scene_string()
+    util.draw_scene(scene_string, my_ax=axs[0,i], dpi=dpi)
+    caxs[0,i].axis('off')
+
+# Label axes and save
+util.label_rows_and_cols(axs, row_labels, col_labels)
+print('Saving final figure.')    
+fig.savefig('../paper/single-frame.pdf', dpi=dpi)
+
+print('Total time: '+str(np.round(time.time() - start, 2)))
+os.system('say "done"')

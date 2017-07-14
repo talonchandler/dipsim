325c6b5dd0ddad4b5550ae1d221d7c9454d68b07
Talon Chandler <talonchandler@talonchandler.com>
2017-07-14 09:52:32 -0500

    Revise single arm plot to sweep NAill and NAdet.

--- paper/figures/single-arm.py
+++ paper/figures/single-arm.py
@@ -2,23 +2,23 @@ from dipsim import multiframe, util
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-import matplotlib.ticker as ticker
+import matplotlib.patches as patches
 import os; import time; start = time.time(); print('Running...')
 import matplotlib.gridspec as gridspec
 
 # Main input parameters
-col_labels = ['Geometry (NA = 0.8)', r'$\sigma_{\Omega}$ [sr]', '', '']
+col_labels = ['Geometry\n(NA${}_{\\textrm{ill}}$ = 0.6, NA${}_{\\textrm{det}}$ = 0.8)', r'$\sigma_{\Omega}$ [sr]', 'Mean${}_{0-99}$$\{\sigma_{\Omega}\}$ [sr]', 'STD${}_{0-99}$$\{\sigma_{\Omega}\}$ [sr]']
 fig_labels = ['a)', 'b)', 'c)', 'd)']
-n_pts = 500 # Points on sphere
+n_pts = 100 # Points on sphere
 n_pts_sphere = 25000 # Points on sphere
-n_grid_pts = 30
+n_grid_pts = 25
 inch_fig = 5
 dpi = 300
 percentile = 99
 
 # Setup figure and axes
 fig = plt.figure(figsize=(2.2*inch_fig, 2*inch_fig))
-gs0 = gridspec.GridSpec(2, 2, wspace=0.2, hspace=0.1)
+gs0 = gridspec.GridSpec(2, 2, wspace=0.4, hspace=0.1)
 gs00 = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=gs0[0,0], width_ratios=[1, 0.05], wspace=0.1)
 gs10 = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=gs0[1,0], width_ratios=[1, 0.05], wspace=0.1)
 gs01 = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=gs0[0,1], width_ratios=[1, 0.05], wspace=0.1)
@@ -29,15 +29,15 @@ ax2 = plt.subplot(gs10[0])
 ax3 = plt.subplot(gs11[0])
 cax0 = plt.subplot(gs00[1]); cax0.axis('off');
 cax1 = plt.subplot(gs01[1])
-cax2 = plt.subplot(gs10[1]); cax2.axis('off');
-cax3 = plt.subplot(gs11[1]); cax3.axis('off');
+cax2 = plt.subplot(gs10[1])
+cax3 = plt.subplot(gs11[1])
 
 for ax, col_label, fig_label  in zip([ax0, ax1, ax2, ax3], col_labels, fig_labels):
     ax.annotate(col_label, xy=(0,0), xytext=(0.5, 1.05), textcoords='axes fraction',
-                va='center', ha='center', fontsize=14, annotation_clip=False)
+                va='bottom', ha='center', fontsize=14, annotation_clip=False)
     ax.annotate(fig_label, xy=(0,0), xytext=(0, 1.05), textcoords='axes fraction',
-                va='center', ha='center', fontsize=14, annotation_clip=False)
-
+                va='bottom', ha='center', fontsize=14, annotation_clip=False)
+    
 for ax in [ax0, ax1, ax2, ax3]:
     ax.tick_params(axis='both', labelsize=14)
 for cax in [cax1, cax2, cax3]:
@@ -45,27 +45,51 @@ for cax in [cax1, cax2, cax3]:
 
 # Calculate a list of points to sample in region
 n = 1.33
-nas = np.linspace(0.01, 1.33, num=n_grid_pts)
+NA_ill = np.linspace(0, 1.33, num=1000)
+NA_det = NA_ill
+pts = np.mgrid[n/n_grid_pts/2:n-n/n_grid_pts/2:n_grid_pts*1j,n/n_grid_pts/2:n-n/n_grid_pts/2:n_grid_pts*1j].reshape((2, n_grid_pts**2)).T.tolist()
+
+def is_feasible(pt):
+    if pt[0] < pt[1] + 0.2:
+        return True
+    else:
+        return False
+
+pts_list = [pt for pt in pts if is_feasible(pt)]
+pts = np.array(pts_list).T
 
 # Calculate mean and variance for each point
-mean = []
-std = []
-for i, na in enumerate(nas):
-    print('Calculating microscope '+str(i+1)+'/'+str(len(nas)))
+def calc_stats(param):
+    na_ill = param[0]
+    na_det = param[1]
     exp = multiframe.MultiFrameMicroscope(ill_thetas=[0], det_thetas=[0],
-                                      ill_nas=[na], det_nas=[na],
+                                      ill_nas=[na_ill], det_nas=[na_det],
                                       ill_types=['wide'], det_types=['lens'],
                                       colors=['(1,0,0)'], n_frames=4,
                                       n_pts=n_pts, max_photons=1000, n_samp=1.33)
 
     exp.calc_estimation_stats()
     data = exp.sa_uncert[exp.sa_uncert < np.percentile(exp.sa_uncert, percentile)]
-    
-    mean.append(np.mean(data))
-    std.append(np.std(data))
+    return np.mean(data), np.std(data)
 
-# Plot 1D regions
-def plot_1d_regions(ax, nas, data, special_pt=(0.8,0), y_string=''):
+mean = []
+std = []
+for i, pt in enumerate(pts.T):
+    print('Calculating microscope '+str(i+1)+'/'+str(pts.shape[1]))
+    x = calc_stats(pt)
+    mean.append(x[0])
+    std.append(x[1])
+
+# Plot 2D regions
+def plot_2d_regions(ax, cax, pts, data, special_pt=(-1,-1)):
+    ax.plot(NA_ill, NA_det, 'k-', zorder=11)
+
+    # Set y ticks
+    from matplotlib.ticker import FuncFormatter, FixedLocator
+    ax.set_yticks([0, 0.25, 0.5, 0.75, 1.0, 1.33])
+    ax.set_yticklabels(['0', '0.25', '0.5', '0.75', '1.0', '1.33'])
+    ax.set_xticks([0, 0.25, 0.5, 0.75, 1.0, 1.33])
+    ax.set_xticklabels(['0', '0.25', '0.5', '0.75', '1.0', '1.33'])
 
     # Annotation
     def my_annotate(ax, annotation, xy, fontsize=9, rotation=0):
@@ -73,26 +97,56 @@ def plot_1d_regions(ax, nas, data, special_pt=(0.8,0), y_string=''):
                     va='center', ha='center', fontsize=fontsize,
                     annotation_clip=False, rotation=rotation, zorder=13)
 
-    my_annotate(ax, 'NA', (0.5, -0.10), fontsize=14)
-    my_annotate(ax, y_string, (-0.18, 0.5), fontsize=14, rotation=90)
+    my_annotate(ax, 'NA${}_{\\textrm{ill}}$', (0.5, -0.15), fontsize=14)
+    my_annotate(ax, 'NA${}_{\\textrm{det}}$', (-0.18, 0.5), fontsize=14, rotation=90)
 
-    # Plot
-    ax.plot(nas, data, 'k-')
-    ax.plot(special_pt[0], special_pt[1], 'kx', markersize=5, zorder=14)
-    ax.set_yscale('log')
-    ax.set(xlim=[0, 1.33], ylim=[1e-3, 1e1])
-    ax.xaxis.set_ticks([0, 0.25, 0.5, 0.75, 1.00, 1.33])
-    ax.set_xticklabels(['0', '0.25', '0.5', '0.75', '1.0', '1.33'])
+    # Calculate colors
+    color_map='coolwarm'
+    color_norm='log'
+    color_min=1e-3
+    color_max=1e0
+    if color_norm == 'linear':
+        norm = matplotlib.colors.Normalize(vmin=color_min, vmax=color_max)
+    elif color_norm == 'log':
+        norm = matplotlib.colors.LogNorm(vmin=color_min, vmax=color_max)
+    elif color_norm == 'linlog':
+        norm = matplotlib.colors.SymLogNorm(linthresh=linthresh, vmin=-color_max, vmax=color_max)
+    elif color_norm == 'power':
+        norm = matplotlib.colors.PowerNorm(gamma=gamma, vmin=data.min(), vmax=data.max())
+
+    norm_data = norm(data).data
+    norm_data2 = np.expand_dims(norm_data, 1)    
+    cmap = matplotlib.cm.get_cmap(color_map)
+    colors = np.apply_along_axis(cmap, 1, norm_data2)
+
+    # Plot scatter for colorbar
+    sc = ax.scatter(pts[0,:], pts[1,:], c=data, s=0, cmap=cmap, norm=norm,
+                    marker='s', lw=0)
+
+    ax.plot(special_pt[0], special_pt[1], 'kx', markersize=5, zorder=15)
+
+    # Plot patches
+    width = n/(n_grid_pts)
+    height = width
+    for i, (pt, c) in enumerate(zip(pts_list, colors)):
+        ax.add_patch(patches.Rectangle((pt[0] - width/2, pt[1] - height/2), width, height, facecolor=c, edgecolor=c))
+    fig.colorbar(sc, cax=cax, orientation='vertical')
+
+    # Mask around lines
+    ax.fill_between(NA_ill, NA_det, 0, color='white', zorder=2)
+    ax.set(xlim=[0, 1.33], ylim=[0, 1.33])
 
 # Plot first two columns
-na = 0.8
+na_ill = 0.6
+na_det = 0.8
 exp = multiframe.MultiFrameMicroscope(ill_thetas=[0], det_thetas=[0],
-                                  ill_nas=[na], det_nas=[na],
-                                  ill_types=['wide'], det_types=['lens'],
-                                  colors=['(1,0,0)'], n_frames=4,
-                                  n_pts=n_pts_sphere, max_photons=1000, n_samp=1.33)
-
+                                      ill_nas=[na_ill], det_nas=[na_det],
+                                      ill_types=['wide'], det_types=['lens'],
+                                      colors=['(1,0,0)'], n_frames=4,
+                                      n_pts=n_pts_sphere, max_photons=1000, n_samp=1.33)
 exp.calc_estimation_stats()
+
+# Make scene string
 scene_string = exp.scene_string()
 util.draw_scene(scene_string, my_ax=ax0, dpi=dpi)
 util.plot_sphere(directions=exp.directions, data=exp.sa_uncert,
@@ -100,10 +154,10 @@ util.plot_sphere(directions=exp.directions, data=exp.sa_uncert,
                  color_min=None, color_max=None,
                  my_ax=ax1, my_cax=cax1)
     
-# Plots last row
-plot_1d_regions(ax2, nas, mean, special_pt=(na, 0.0054), y_string='Mean${}_{0-99}$$\{\sigma_{\Omega}\}$ [sr]')
-plot_1d_regions(ax3, nas, std, special_pt=(na, 0.0092), y_string='STD${}_{0-99}$$\{\sigma_{\Omega}\}$ [sr]')
-
+# Plots last two columns
+plot_2d_regions(ax2, cax2, pts, mean, special_pt=(na_ill, na_det))
+plot_2d_regions(ax3, cax3, pts, std, special_pt=(na_ill, na_det))
+    
 # Label axes and save
 print('Saving final figure.')    
 fig.savefig('../paper/single-arm.pdf', dpi=250)
